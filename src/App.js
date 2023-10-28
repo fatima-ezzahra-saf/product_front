389488dc8768c65932bf8c165c7396c017ecd08f
SAFRANI Fatima Ezzahra <fatimaezzahrasafrani@gmail.com>
2023-10-28 14:24:57 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './Acceuil/HomePage';
+import AddProduct from './ProductType/AddProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path="/"><Home/></Route>
+      <Route path="/ajouter_un_produit"><AddProduct/></Route>
+      <Route path="/ajouter_un_type"><Home/></Route>
+      <Route path="/afficher_un_type"><Home/></Route>
+    </Switch>
+    </Router>
   );
 }
 
